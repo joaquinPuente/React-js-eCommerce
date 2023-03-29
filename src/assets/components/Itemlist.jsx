44494b49c1ea2866769d6e1f4f8e545ed0a9d0e7
joaquinPuente <joaquinpuentecoronel@gmail.com>
2023-03-28 22:04:47 -0300

    Agregue un Dropdown

--- src/assets/components/Itemlist.jsx
+++ src/assets/components/Itemlist.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import '../../App.css'
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Itemlist = ( {greeting} ) => {
   return (
     <div>
         <ul className='itemList'>
             <li>{greeting}</li>
-            <li>Categorias</li>
             <li>Todo</li>
+            <Dropdown>
+
+              <Dropdown.Toggle variant="light" id="dropdown-basic">
+                Categorias
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Urbanas</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Deportivas</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Casuales</Dropdown.Item>
+              </Dropdown.Menu>
+
+            </Dropdown>
         </ul>
-        
+       
     </div>
   )
 }
